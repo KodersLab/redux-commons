f0a63eee0672f86f0e328108e1584110e4bfe6d4
mattiamanzati <manzati.mattia@gmail.com>
2016-04-28 19:10:31 +0200

    initialize map

---
+++ src/map-of.js
@@ -0,0 +1,17 @@
+/**
+ * Given a reducer, returns a reducer which manages a keyed object of that value. Useful for usersById etc...
+ * @param {bool} stripUndefinedItems Whenever to automatically remove undefined items from the collection.
+ */
+export default function (stripUndefinedItems = true){
+    return reducer => 
+        (state = {}, action) => {
+            var newState = {};
+            Object.keys(state).forEach(key => {
+                var value = reducer(state[key], action);
+                if(!(stripUndefinedItems && value != undefined) || !stripUndefinedItems){
+                    newState[key] = value;
+                }
+            });
+            return newState;
+        };
+}
