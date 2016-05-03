7037afe8e9cc21be5d695ce78e0a27069b1473fc
mattiamanzati <mattia.manzati@gmail.com>
2016-05-03 21:23:43 +0200

    rename ducks to lib

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import collectionOf from './collection-of';
+import mapOf from './map-of';
+import withDefaults from './with-defaults';
+import withScope, {applyScope} from './with-scope';
+
+export {
+    collectionOf,
+    mapOf,
+    withDefaults,
+    withScope,
+    applyScope
+};
